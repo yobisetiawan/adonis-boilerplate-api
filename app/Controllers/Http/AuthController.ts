e67179cd54ddd62618952fda8c201425efa05692
Yobi Bina Setiawan <yobi.studio@gmail.com>
2021-06-20 06:24:31 +0700

    change var to http

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -9,15 +9,15 @@ export default class AuthController {
     this.repo = new AuthRepository()
   }
 
-  public async login(req: HttpContextContract) {
-    return this.repo.loginProcess(req)
+  public async login(http: HttpContextContract) {
+    return this.repo.loginProcess(http)
   }
 
-  public async logout(req: HttpContextContract) {
-    return this.repo.logoutProcess(req)
+  public async logout(http: HttpContextContract) {
+    return this.repo.logoutProcess(http)
   }
 
-  public async register(req: HttpContextContract) {
-    return this.repo.DBSafe(() => this.repo.registerProcess(req))
+  public async register(http: HttpContextContract) {
+    return this.repo.DBSafe(() => this.repo.registerProcess(http))
   }
 }
